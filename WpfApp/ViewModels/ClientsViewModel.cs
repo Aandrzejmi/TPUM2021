c96f90b8fffa114c54de95741d024af4658fd771
Aandrzejmi <andrzej.mi@op.pl>
2021-04-18 00:09:55 +0200

    Pages - proof of concept

--- WpfApp/ViewModels/ClientsViewModel.cs
+++ WpfApp/ViewModels/ClientsViewModel.cs
@@ -1,13 +1,23 @@
-﻿using System;
+﻿using LogicAPI.DTOs;
+using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.ComponentModel;
 using System.Text;
+
 
 namespace WpfApp.ViewModels
 {
     class ClientsViewModel : INotifyPropertyChanged
     {
         private string _test = "Clients";
+        public ObservableCollection<ClientDTO> Clients { get; set; } = new ObservableCollection<ClientDTO>()
+        {
+            new ClientDTO(){ID = 1, Name = "Jan Kowalski", Adress = "Południowa 23"},
+            new ClientDTO(){ID = 2, Name = "Anna Nowak", Adress = "Złota 15b"},
+            new ClientDTO(){ID = 3, Name = "Alicja Makota ", Adress = "Długa 281 mieszkania 12"},
+        };
+
         public string TestField
         {
             get => _test;
@@ -17,6 +27,7 @@ namespace WpfApp.ViewModels
                 PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("TestField"));
             }
         }
+
 
         public event PropertyChangedEventHandler PropertyChanged;
     }
