9c1b72a28e37edd58157615345d844278e88e0a5
Aandrzejmi <andrzej.mi@op.pl>
2021-04-17 21:47:07 +0200

    Target viewmodel structure

---
+++ WpfApp/ViewModels/OrdersViewModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+
+namespace WpfApp.ViewModels
+{
+    class OrdersViewModel : INotifyPropertyChanged
+    {
+        private string _test = "Orders";
+        public string TestField
+        {
+            get => _test;
+            set
+            {
+                _test = value;
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("TestField"));
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+    }
+}
---
+++ WpfApp/ViewModels/ProductsViewModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+
+namespace WpfApp.ViewModels
+{
+    class ProductsViewModel : INotifyPropertyChanged
+    {
+        private string _test = "Products";
+        public string TestField
+        {
+            get => _test;
+            set
+            {
+                _test = value;
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("TestField"));
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+    }
+}
