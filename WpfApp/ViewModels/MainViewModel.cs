812ee56892ff6ad9f20e203ef2e2c00076659bf7
Aandrzejmi <andrzej.mi@op.pl>
2021-04-17 18:57:15 +0200

    one view model

--- WpfApp/ViewModels/MainViewModel.cs
+++ WpfApp/ViewModels/MainViewModel.cs
@@ -1,22 +1,58 @@
-﻿using System;
+﻿using LogicAPI;
+using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.ComponentModel;
 using System.Text;
 using System.Windows.Input;
+using WpfApp.Commands;
 
 namespace WpfApp.ViewModels
 {
-    public class MainViewModel
+    public class MainViewModel : INotifyPropertyChanged
     {
-        public MainViewModel()
+        public static string defaultName = "Entern new name";
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public ICommand AddNewVM { get; set; }
+        public ICommand ButtonTest { get; private set; }
+
+        public ObservableCollection<ExampleDTO> Collection { get; set; }
+
+        public string NewName
         {
-            InputVM = new InputViewModel(Collection);
-            ButtonTest = new ButtonTestCommand(Collection);
+            get => _newName;
+            set
+            {
+                _newName = value;
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("NewName"));
+            }
         }
 
-        public InputViewModel InputVM { get; set; } // initialized in constructor
-        public ICommand AddNewVM => InputVM.TryAdd;
-        public ObservableCollection<ExampleViewModel> Collection { get; set; } = ExampleViewModel.Col();
-        public ButtonTestCommand ButtonTest { get; private set; }
+        public int NewAge
+        {
+            get => _newAge;
+            set
+            {
+                _newAge = value;
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("NewAge"));
+            }
+        }
+
+
+        public MainViewModel()
+        {
+            Collection = new ObservableCollection<ExampleDTO>();
+            Collection.Add(new ExampleDTO() { Name = "Anna Nowak", Age = 15 });
+            Collection.Add(new ExampleDTO() { Name = "Jan Kowalski", Age = 34 });
+            Collection.Add(new ExampleDTO() { Name = "Zygmunt Szmidt", Age = 66 });
+
+            ButtonTest = new ButtonTestCommand(Collection);
+            AddNewVM = new AddNewViewModellCommand(this);
+        }
+
+        private string _newName = defaultName;
+        private int _newAge;
     }
 }
