23769cc3814d6f0eb33b49716915ec57e9a77ef1
Sharpyfile <44209792+Sharpyfile@users.noreply.github.com>
2021-04-18 22:38:13 +0200

    Create Thread Commands for Orders and Products

---
+++ WpfApp/Commands/AddOrderThreadCommand.cs
@@ -0,0 +1,50 @@
+﻿using LogicAPI;
+using LogicAPI.DTOs;
+using LogicAPI.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using System.Windows.Input;
+
+namespace WpfApp.Commands
+{
+    class AddOrderThreadCommand : ICommand
+    {
+        private static int index = 0;
+        private IOrderService _orderService = Logic.CreateOrderService();
+        private IEvidenceEntryService _evidenceEntryService = Logic.CreateEvidenceEntryService();
+        private IClientService _clientService = Logic.CreateClientService();
+        private Thread _thread;
+
+        public event EventHandler CanExecuteChanged;
+
+        public bool CanExecute(object parameter) => true;
+
+        public void Execute(object parameter)
+        {
+            if (_thread == null)
+            {
+                _thread = new Thread(AddOrdersLoop);
+                _thread.Start();
+            }
+            else
+            {
+                _thread.Abort();
+                _thread = null;
+            }
+        }
+
+        private EvidenceEntryDTO CreateEvidenceEntryDTO() => _evidenceEntryService.GetEvidenceEntryDTOByID(0);
+        private OrderDTO CreateDTO() => new OrderDTO() {  Products = new List<EvidenceEntryDTO>() { CreateEvidenceEntryDTO() }, Client = _clientService.GetClientDTOByID(0) };
+
+        private void AddOrdersLoop()
+        {
+            while (true)
+            {
+                _orderService.AddOrderDTO(CreateDTO());
+                Thread.Sleep(30000);
+            }
+        }
+    }
+}
---
+++ WpfApp/Commands/AddProductThreadCommand.cs
@@ -0,0 +1,47 @@
+﻿using LogicAPI;
+using LogicAPI.DTOs;
+using LogicAPI.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using System.Windows.Input;
+
+namespace WpfApp.Commands
+{
+    class AddProductThreadCommand : ICommand
+    {
+        private static int index = 0;
+        private IProductService _productService = Logic.CreateProductService();
+        private Thread _thread;
+
+        public event EventHandler CanExecuteChanged;
+
+        public bool CanExecute(object parameter) => true;
+
+        public void Execute(object parameter)
+        {
+            if (_thread == null)
+            {
+                _thread = new Thread(AddProductsLoop);
+                _thread.Start();
+            }
+            else
+            {
+                _thread.Abort();
+                _thread = null;
+            }
+        }
+
+        private ProductDTO CreateDTO() => new ProductDTO() { Price = 2.0M, Name = $"New product {index++}" };
+
+        private void AddProductsLoop()
+        {
+            while (true)
+            {
+                _productService.AddProductDTO(CreateDTO());
+                Thread.Sleep(30000);
+            }
+        }
+    }
+}
