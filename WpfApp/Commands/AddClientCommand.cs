bbd54b0bcccd614133453c3df194752e5ad22360
Aandrzejmi <andrzej.mi@op.pl>
2021-04-18 17:17:27 +0200

    Add Client proof of concept - lack of logic layer events

---
+++ WpfApp/Commands/AddClientCommand.cs
@@ -0,0 +1,41 @@
+﻿using LogicAPI.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Input;
+using WpfApp.ViewModels;
+
+namespace WpfApp.Commands
+{
+    class AddClientCommand : ICommand
+    {
+        private readonly NewClientViewModel _vm;
+        private readonly IClientService _service;
+
+        public AddClientCommand(NewClientViewModel vm, IClientService service)
+        {
+            _vm = vm;
+            _service = service;
+        }
+
+        public event EventHandler CanExecuteChanged
+        {
+            add { CommandManager.RequerySuggested += value; }
+            remove { CommandManager.RequerySuggested -= value; }
+        }
+
+        public bool CanExecute(object parameter)
+        {
+            return _vm.Name != NewClientViewModel.defaultName
+                && _vm.Adress != NewClientViewModel.defaultAddres
+                && _vm.Name.Length > 1
+                && _vm.Adress.Length > 1;
+        }
+
+        public void Execute(object parameter)
+        {
+            _service.AddClientDTO(_vm.CreateDTO());
+            _vm.ResetFields();
+        }
+    }
+}
---
+++ WpfApp/ViewModels/NewClientViewModel.cs
@@ -0,0 +1,71 @@
+﻿using LogicAPI;
+using LogicAPI.DTOs;
+using LogicAPI.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+using System.Windows.Input;
+using WpfApp.Commands;
+
+namespace WpfApp.ViewModels
+{
+    class NewClientViewModel : INotifyPropertyChanged
+    {
+        public const string defaultName = "Enter name";
+        public const string defaultAddres = "Enter adress";
+
+        private IClientService _clientService;
+        private string _name;
+        private string _adress;
+
+        public ICommand Add { get; set; }
+
+        public NewClientViewModel()
+        {
+            _clientService = Logic.CreateClientService();
+            Add = new AddClientCommand(this, _clientService);
+            ResetFields();
+        }
+
+        public ClientDTO CreateDTO()
+        {
+            return new ClientDTO()
+            {
+                ID = (new Random()).Next(1, int.MaxValue),
+                Name = _name,
+                Adress = _adress,
+            };
+        }
+
+        public string Name
+        {
+            get => _name;
+            set
+            {
+                _name = value;
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("Name"));
+            }
+        }
+
+        public string Adress
+        {
+            get => _adress;
+            set
+            {
+                _adress = value;
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("Adress"));
+            }
+        }
+
+        public void ResetFields()
+        {
+            Name = defaultName;
+            Adress = defaultAddres;
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+
+    }
+}
