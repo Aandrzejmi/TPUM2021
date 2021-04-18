c617dd0447fd90ef1060f8e4caf3fdca6ceaad0c
Sharpyfile <44209792+Sharpyfile@users.noreply.github.com>
2021-04-18 21:41:54 +0200

    Add simple setup for another thread creating Client every 30 seconds

--- LogicAPI/Logic.cs
+++ LogicAPI/Logic.cs
@@ -1,8 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Text;
+using System.Threading;
 using LogicAPI.Interfaces;
 using LogicAPI.Services;
+using LogicAPI.DTOs;
 using DataAPI;
 
 namespace LogicAPI
@@ -27,6 +28,22 @@ namespace LogicAPI
         public static IEvidenceEntryService CreateEvidenceEntryService()
         {
             return new EvidenceEntryService(Data.GetRepository());
+        }
+
+        public static void NewThreadAddClient()
+        {
+            IClientService clientService = CreateClientService();
+            System.Console.WriteLine("Waiting 5 seconds before adding new client");
+            Thread.Sleep(5000);
+            ClientDTO clientDTO1 = new ClientDTO() { ID = 0, Adress = "Temporary Adress", Name = "Temporary Name" };
+            clientService.AddClientDTO(clientDTO1);
+            while (true)
+            {
+                System.Console.WriteLine("Waiting 30 seconds before adding new client");
+                ClientDTO clientDTO2 = new ClientDTO() { ID = 0, Adress = "Latter adress", Name = "Latter Name" };
+                Thread.Sleep(30000);
+                clientService.AddClientDTO(clientDTO2);
+            }
         }
     }
 }
