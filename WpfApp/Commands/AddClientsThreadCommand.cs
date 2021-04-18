997fec1582434b1a7e2de8549e8c7a625dc0e0f7
Aandrzejmi <andrzej.mi@op.pl>
2021-04-18 22:26:57 +0200

    thread proof of concept

---
+++ WpfApp/Commands/AddClientsThreadCommand.cs
@@ -0,0 +1,47 @@
+﻿using LogicAPI;
+using LogicAPI.DTOs;
+using LogicAPI.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using System.Windows.Input;
+
+namespace WpfApp.Commands
+{
+    class AddClientsThreadCommand : ICommand
+    {
+        private static int index = 0;
+        private IClientService _clientService = Logic.CreateClientService();
+        private Thread _thread;
+
+        public event EventHandler CanExecuteChanged;
+
+        public bool CanExecute(object parameter) => true;
+
+        public void Execute(object parameter)
+        {
+            if (_thread == null)
+            {
+                _thread = new Thread(AddClientsLoop);
+                _thread.Start();
+            }
+            else
+            {
+                _thread.Abort();
+                _thread = null;
+            }
+        }
+
+        private ClientDTO CreateDTO() => new ClientDTO() { Adress = "Temporary Adress", Name = $"New client {index++}" };
+
+        private void AddClientsLoop()
+        {
+            while (true)
+            {
+                _clientService.AddClientDTO(CreateDTO());
+                Thread.Sleep(30000);
+            }
+        }
+    }
+}
