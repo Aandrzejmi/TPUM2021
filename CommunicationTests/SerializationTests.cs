98c9c312f4358e92a59e1ceeb202336fec4b5c8e
Aandrzejmi <andrzej.mi@op.pl>
2021-05-29 21:29:10 +0200

    CSendRequest

--- CommunicationTests/SerializationTests.cs
+++ CommunicationTests/SerializationTests.cs
@@ -12,6 +12,7 @@ namespace CommunicationTests
         CProduct product;
         CEvidenceEntry evEntry1;
         CEvidenceEntry evEntry2;
+        CSendRequest request;
         List<CEvidenceEntry> list;
 
         string clientJSON;
@@ -19,13 +20,17 @@ namespace CommunicationTests
         string productJSON;
         string evEntry1JSON;
         string evEntry2JSON;
+        string requestJSON;
         string listJSON;
+
 
         [SetUp]
         public void Setup()
         {
             client = new CClient() { ID = 1, Name = "Grzegorz Brzêczyszczykiewicz", Adress = "Chrz¹szczyrzew¹szczyce 21" };
             product = new CProduct() { ID = 1, Name = "Cement (10kg)", Price = 123.45m };
+
+            request = new CSendRequest() { Type = typeof(CClient).ToString(), RequestedID = null };
 
             evEntry1 = new CEvidenceEntry() { Product = product, Amount = 7 };
             evEntry2 = new CEvidenceEntry() { Product = product, Amount = 8 };
@@ -37,12 +42,15 @@ namespace CommunicationTests
             clientJSON = "{\"__type\":\"CClient:#CommunicationAPI.Models\",\"Adress\":\"Chrz¹szczyrzew¹szczyce 21\",\"ID\":1,\"Name\":\"Grzegorz Brzêczyszczykiewicz\"}";
             productJSON = "{\"__type\":\"CProduct:#CommunicationAPI.Models\",\"ID\":1,\"Name\":\"Cement (10kg)\",\"Price\":123.45}";
 
+            productJSON = "{\"__type\":\"CProduct:#CommunicationAPI.Models\",\"ID\":1,\"Name\":\"Cement (10kg)\",\"Price\":123.45}";
+
             evEntry1JSON = "{\"__type\":\"CEvidenceEntry:#CommunicationAPI.Models\",\"Amount\":7,\"Product\":" + productJSON + "}";
             evEntry2JSON = "{\"__type\":\"CEvidenceEntry:#CommunicationAPI.Models\",\"Amount\":8,\"Product\":" + productJSON + "}";
             orderJSON = "{\"__type\":\"COrder:#CommunicationAPI.Models\",\"Client\":" + clientJSON + ",\"Entries\":[" + evEntry1JSON + "," + evEntry2JSON + "],\"ID\":1}";
 
-            listJSON = $"[{evEntry1JSON},{evEntry2JSON},{evEntry1JSON}]";
+            requestJSON = "{\"__type\":\"CSendRequest:#CommunicationAPI.Models\",\"RequestedID\":null,\"Type\":\"" + typeof(CClient) + "\"}";
 
+            listJSON = $"[{evEntry1JSON},{evEntry2JSON},{evEntry1JSON}]";
         }
 
         [Test]
@@ -50,6 +58,7 @@ namespace CommunicationTests
         {
             Assert.AreEqual(clientJSON, Serialize(client));
             Assert.AreEqual(productJSON, Serialize(product));
+            Assert.AreEqual(requestJSON, Serialize(request));
         }
 
         [Test]
@@ -70,6 +79,7 @@ namespace CommunicationTests
         {
             Assert.AreEqual(client, Deserialize<CClient>(clientJSON));
             Assert.AreEqual(product, Deserialize<CProduct>(productJSON));
+            Assert.AreEqual(request, Deserialize<CSendRequest>(requestJSON));
         }
 
         [Test]
